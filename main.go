9ad0cf63e3019271174d2a077d9a7d2f0f45f563
LoreviQ <lorevi@protonmail.com>
2024-03-06 21:28:57 +0000

    Created main exectuion loop to read commands and exectute them

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type cliCommand struct {
@@ -12,16 +13,55 @@ type cliCommand struct {
 	callback    func() error
 }
 
+func getCommand(word string) (cliCommand, error) {
+	commands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    callHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    callExit,
+		},
+	}
+	command, ok := commands[word]
+	if ok {
+		return command, nil
+	} else {
+		var zeroVal cliCommand
+		return zeroVal, fmt.Errorf("%s is an invalid command", word)
+	}
+}
+
+func callHelp() error {
+	_, ok := fmt.Println("Help was Called")
+	return ok
+}
+
+func callExit() error {
+	_, ok := fmt.Println("Exit was Called")
+	return ok
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Print("-> ")
-		command, err := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Print(command)
+		input = strings.Replace(input, "\n", "", -1)
+		words := strings.Split(input, " ")
+		command, err := getCommand(words[0])
+		if err == nil {
+			command.callback()
+		} else {
+			fmt.Println(err)
+		}
 	}
 }
