589ba78f58b152093f7dc86da229d75c099d5159
LoreviQ <lorevi@protonmail.com>
2024-03-07 20:15:39 +0000

    Removes PokeGo exectuable from repo

--- apiHandler.go
+++ apiHandler.go
@@ -18,6 +18,28 @@ type APImapData struct {
 	} `json:"results"`
 }
 
+func getEndpoint(config config, args []string) (string, error) {
+	forward := true
+	if len(args) != 0 && (args[0] == "-back" || args[0] == "-b") {
+		forward = false
+	}
+	var endpoint string
+	if forward {
+		if config.Next == "" {
+			endpoint = "https://pokeapi.co/api/v2/location/"
+		} else {
+			endpoint = config.Next
+		}
+	} else {
+		if config.Previous == "" {
+			return "", errors.New("cannot go backwards from the start")
+		} else {
+			endpoint = config.Previous
+		}
+	}
+	return endpoint, nil
+}
+
 func getAPI(endpoint string) ([]byte, error) {
 	res, err := http.Get(endpoint)
 	if err != nil {
@@ -42,26 +64,4 @@ func convertToStruct(body []byte) (APImapData, error) {
 		return zeroVal, err
 	}
 	return JSON, nil
-}
-
-func getEndpoint(config config, args []string) (string, error) {
-	forward := true
-	if len(args) != 0 && (args[0] == "-back" || args[0] == "-b") {
-		forward = false
-	}
-	var endpoint string
-	if forward {
-		if config.Next == "" {
-			endpoint = "https://pokeapi.co/api/v2/location/"
-		} else {
-			endpoint = config.Next
-		}
-	} else {
-		if config.Previous == "" {
-			return "", errors.New("cannot go backwards from the start")
-		} else {
-			endpoint = config.Previous
-		}
-	}
-	return endpoint, nil
 }
